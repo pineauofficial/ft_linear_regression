a7527f0985c9e0a8afec6bc72151e40e613cc1cc
pineau <pierrickn44@gmail.com>
2024-06-07 01:44:20 +0200

    collecte de data

--- tmp.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.plot([0, 1, 2, 3, 4, 5, 6, 7], [2, 4, 6, 8, 10, 12, 14, 16], 'r+')
-
-plt.axis((0, 10, 0, 20))
-plt.ylabel('axe y')
-plt.xlabel('axe x')
-
-plt.show()
--- trainning.py
+++ trainning.py
@@ -11,6 +11,10 @@ tmpθ0 = learningRate * (1 / m) * estimatedPrice(km - price)
 tmpθ1 = learningRate * (1 / m) * (estimatedPrice(km - price) * km)
 
 ---> θ0 et θ1 sont les led parametres de la regression lineaire
----> y = θ0 + θ1 * x
+---> y = θ0 + θ1 * x      /      y = θ1 * x + θ0
 	---> y est le prix estimé
 	---> x est le kilométrage
+
+x sera le meme pour les points connu et ceux de la droite
+R = a la difference entre le y de la droite et le y du point connu au carre
+S = somme de tous les R residus (nombre qui devras etre le plus petit possible) 
