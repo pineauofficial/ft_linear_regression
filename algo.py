e4770e519520fcc927ba912efcc0d31810b657eb
Pierrick NEAU <pierrickn44@gmail.com>
2024-06-24 15:08:27 +0200

    algo done + display graph no animation

--- algo.py
+++ algo.py
@@ -1,38 +1,80 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import pandas as pd
 
-#from data_init import km, price
-km = [240000, 139800, 150500]
-price = [3650, 3800, 4400]
-learningRate = 0.01
+sys.dont_write_bytecode = True
+
+data = pd.read_csv('data.csv')
+km = data['km']
+price = data['price']
+
+learningRate = 0.1
 T0 = 0
 T1 = 0
 m = len(km)
+km_value = int(sys.argv[1])
 
-#calcul de la somme des erreurs
+def normalize(data):
+    min_val = min(data)
+    max_val = max(data)
+    normalized_data = []
+    for x in data:
+        normalized_value = (x - min_val) / (max_val - min_val)
+        normalized_data.append(normalized_value)
+    return normalized_data
+
 def sumError():
 	sum = 0
 	for i in range(m):
-		sum += (T0 + T1 * km[i]) - price[i]
+		sum += (T0 + T1 * km_normalized[i]) - price_normalized[i]
 	return sum
 
-#calcul de tmpT0
 def tmpTheta0():
 	return learningRate * (1/m) * sumError()
 
-#calcul de tmpT1
 def tmpTheta1():
 	sum = 0
 	for i in range(m):
-		sum += ((T0 + T1 * km[i]) - price[i]) * km[i]
+		sum += ((T0 + T1 * km_normalized[i]) - price_normalized[i]) * km_normalized[i]
 	return learningRate * (1/m) * sum
 
-iteration = 10
-for i in range(iteration):
-	T0 = T0 - tmpTheta0()
-	T1 = T1 - tmpTheta1()
-	print("sumError:", sumError())
-	print("tmpTheta0:", tmpTheta0())
-	print("tmpTheta1:", tmpTheta1())
-	print("y = ", T0 + T1 * 240000)
+def display_graph():
+	plt.plot(km, y_hat, 'b-', label='linear regression')
+	plt.plot(km, price, 'r+', label='Data Points')
+	plt.plot(km_value, predicted_price, 'ro', label='Prediction')
 
+	plt.axis([min(km) * 0.9, max(km) * 1.1, min(price) * 0.9, max(price) * 1.1])
+	plt.xlabel('km')
+	plt.ylabel('price')
+	plt.title('Price vs. km')
+	plt.legend()
+
+km_normalized = normalize(km)
+price_normalized = normalize(price)
+
+iteration = 1000
+for i in range(iteration):
+    tmp_T0 = tmpTheta0()
+    tmp_T1 = tmpTheta1()
+    
+    T0 -= tmp_T0
+    T1 -= tmp_T1
+
+    km_value_normalized = (km_value - min(km)) / (max(km) - min(km))
+    predicted_price_normalized = T0 + T1 * km_value_normalized
+
+    min_price = min(price)
+    max_price = max(price)
+    predicted_price = predicted_price_normalized * (max_price - min_price) + min_price
+    y_hat = []
+    for i in range(len(km)):
+        y_hat.append((T0 + T1 * km_normalized[i]) * (max_price - min_price) + min_price)
+
+
+print("y = ", predicted_price, "pour km = " + sys.argv[1])
+display_graph()
+plt.show()
+
+
+#-- = pointilles
--- data_init.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('data.csv')
-km = data['km']
-price = data['price']
-
